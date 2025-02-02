7e1d47cebc037e9f9fe163f722e78e6e8f86d1ee
Russel Fs <russelfloressolano900@gmail.com>
2025-02-02 15:48:47 +0000

    Se implementó CorsConfig

---
+++ src/main/java/com/hexagonal_service/hexagonal_spring_service/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.hexagonal_service.hexagonal_spring_service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://super-engine-96jjpvqrvxh79rq-4200.app.github.dev/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
